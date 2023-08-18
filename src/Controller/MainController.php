41d114544ac66be91ee63e4fe094400a43e10e2d
Etudiant O'clock <bricenantard@gmail.com>
2023-08-18 12:06:22 +0200

    test

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * Homepage
+     * 
+     * @Route("/", name="app_main_home", methods={"GET"})
+     */
+    public function home()
+    {    
+        echo('coucou');
+
+        return $this->render('main/home.html.twig', []);
+    }
+}
+
