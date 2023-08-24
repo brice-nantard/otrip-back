91a9729e7445d18fb78803bd9ebff5d8e97152c3
Etudiant O'clock <bricenantard@gmail.com>
2023-08-24 11:21:03 +0200

    création TripController

---
+++ src/Controller/TripController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TripController extends AbstractController
+{
+    /**
+     * @Route("/api/trip", name="app_trip")
+     */
+    public function index(): Response
+    {
+        return $this->render('trip/index.html.twig', [
+            'controller_name' => 'TripController',
+        ]);
+    }
+}
