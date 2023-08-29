a6972149643349b3b3dab64ed2c9f6d03ba87830
Etudiant O'clock <bricenantard@gmail.com>
2023-08-29 15:22:34 +0200

    test

--- src/Controller/StepController.php
+++ src/Controller/StepController.php
@@ -32,20 +32,6 @@ class StepController extends AbstractController
         );
     }
 
-    /**
-     * @Route("/api/steps/trips/{id})
-     */
-    //public function getStepsFromTrip(StepRepository $stepRepository): JsonResponse
-    //{
-    //    $tripSteps = $stepRepository->findStepsFromTrip();
-
-    //    return $this->json(
-    //        $tripSteps,
-    //        200,
-    //        [],
-    //        ['groups' => 'get_collection']
-    //    );
-    //}
 
     /**
      * Route qui va nous permettre de rajouter un film à l'aide d'une requête HTTP en methode POST
